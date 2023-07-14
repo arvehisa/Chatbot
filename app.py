cef794744873876f86b9a923745b27f80c806cd3
arvehisa <arvehisa@gmail.com>
2023-07-14 17:06:17 +0900

    Added 1705 on title

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def communicate():
 
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.write("ChatGPT APIを使ったチャットボットです。1705")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
